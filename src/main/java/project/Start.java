49d596a078ea3691ed3a16d9399e294a43117b47
AaronStahley <acstahle@asu.edu>
2018-03-29 19:31:09 -0700

    Initial files

---
+++ src/main/java/project/Start.java
@@ -0,0 +1,17 @@
+package main.java.project;
+
+/**
+ * 
+ * @author aaronstahley
+ * @version 1.0
+ * Main class to start project 
+ *
+ */
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
